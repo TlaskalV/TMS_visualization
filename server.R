71a00a6d483d794bb376294d8172929b5c8ea13c
Vojczech <tlaskal@biomed.cas.cz>
2018-07-26 07:38:41 +0200

    add average lines and text

--- server.R
+++ server.R
@@ -48,15 +48,58 @@ function(input, output) {
       gather(position, temp, temp_lower:temp_upper)
   })
   
+  temp_average_upper <- reactive({
+    temp <- summarySE(dataset_temp_filtered(), measurevar = "temp", groupvars = "position", conf.interval = 0.95)
+    temp_upper <- temp[[3,3]]
+  })
+  
+  temp_average_middle <- reactive({
+    temp <- summarySE(dataset_temp_filtered(), measurevar = "temp", groupvars = "position", conf.interval = 0.95)
+    temp_middle <- temp[[2,3]]
+  })
+  
+  temp_average_lower <- reactive({
+    temp <- summarySE(dataset_temp_filtered(), measurevar = "temp", groupvars = "position", conf.interval = 0.95) 
+    temp_lower <- temp[[1,3]]
+  })
+  
   ggplot_final <- reactive({
-    ggplot(data = dataset_temp_filtered(), aes(y = temp, x = date_parsed)) +
-      geom_line(aes(color = position), size = 2) +
+    dataset_temp_filtered <- dataset_temp_filtered()
+    dataset_temp_filtered$position <- factor(dataset_temp_filtered$position, levels = c("temp_upper", "temp_middle", "temp_lower"))
+    temp_average_upper <- temp_average_upper()
+    temp_average_middle <- temp_average_middle()
+    temp_average_lower <- temp_average_lower()
+      ggplot(data = dataset_temp_filtered, aes(y = temp, x = date_parsed)) +
+      {if (input$upper == TRUE) 
+        geom_line(aes(colour = position), size = 2, subset(dataset_temp_filtered, position == "temp_upper"))   
+      } +
+      {if (input$middle == TRUE) 
+        geom_line(aes(colour = position), size = 2, subset(dataset_temp_filtered, position == "temp_middle"))   
+      } +
+      {if (input$lower == TRUE) 
+        geom_line(aes(colour = position), size = 2, subset(dataset_temp_filtered, position == "temp_lower"))   
+      } +
       scale_color_viridis_d() + # color in the case of discrete values    
       scale_x_datetime(date_breaks = "1 day") +
-      labs(title = input$plot_title, subtitle = "average", x = "date", y = "temperature [°C]") +
-      #geom_hline(yintercept = 22.39062, color = "#440154") +
-      #geom_hline(yintercept = 22.40365, color = "#29788E") +
-      #geom_hline(yintercept = 22.39714, color = "#FDE724") +
+      labs(title = input$plot_title, subtitle = paste("mean upper sensor - ", round(temp_average_upper, digits = 1), "°C\n", "mean middle sensor - ", round(temp_average_middle, digits = 1), "°C\n", "mean lower sensor - ", round(temp_average_lower, digits = 1), "°C\n"), x = "date", y = "temperature [°C]") +
+      {if (input$upper == TRUE & input$average == TRUE)
+        geom_hline(yintercept = temp_average_upper, size = 1) 
+      } +
+      {if (input$upper == TRUE & input$average == TRUE)
+        annotate("text", x = as.POSIXct(input$date_range[1] + 0.5), y = temp_average_upper + 0.01, label = "upper sensor", size = 2)
+      } +
+      {if (input$middle == TRUE & input$average == TRUE)
+        geom_hline(yintercept = temp_average_middle, size = 1) 
+      } +
+      {if (input$middle == TRUE & input$average == TRUE)
+          annotate("text", x = as.POSIXct(input$date_range[1] + 0.5), y = temp_average_middle + 0.01, label = "middle sensor", size = 2) 
+      } +
+      {if (input$lower == TRUE & input$average == TRUE)
+        geom_hline(yintercept = temp_average_lower, size = 1) 
+      } +
+      {if (input$lower == TRUE & input$average == TRUE)
+        annotate("text", x = as.POSIXct(input$date_range[1] + 0.5), y = temp_average_lower + 0.01, label = "lower sensor", size = 2) 
+      } +
       theme_bw() +
       theme(axis.text.x = element_text(angle = 90, size = 10, colour = "black"), axis.text.y = element_text(size = 13, colour = "black"), axis.title = element_text(size = 14, face = "bold", colour = "black"), plot.title = element_text(size = 14, face = "bold", colour = "black"),  plot.subtitle = element_text(colour = "black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
   })
@@ -65,7 +108,13 @@ function(input, output) {
     ggplot_final()
   })
   output$contents1 <- renderTable({
-    head(dataset_temp_filtered(), 5)
+    head(temp_average_upper(), 5)
+  })
+  output$contents6 <- renderTable({
+    head(temp_average_middle(), 5)
+  })
+  output$contents7 <- renderTable({
+    head(temp_average_lower(), 5)
   })
   output$contents2 <- renderTable({
     tail(dataset_temp_filtered(), 5)
